3f640afda6e28f57b55635a992f5c56bde0c8d7c
kjaffel <khawla.jaffel@cern.ch>
2025-05-01 17:28:42 +0200

    update code test when install

--- python/getXsecFromSusHi.py
+++ python/getXsecFromSusHi.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python
+
 import os
 import sys
 
 
-def getXsecFromSusHi( mode = 'H', sqrts = 13000, tb = 1, m12 = 0, mh = 125, mH = 350, mA = 350, mhc = 350, sba = 0.99, muR = 0.5, muF = 0.5) :
-    
-    sushiDefaultCardPath = "default_cards/default_sushi_8TeV.in"
+def getXsecFromSusHi( mode = 'H', sqrts = 13000, tb = 1, m12 = 0, mh = 125, mH = 350, mA = 350, 
+        mhc = 350, sba = 0.99, muR4ggh = 0.5, muF4ggh = 0.5, muR4bbh = 1., muR4bbh = 0.25):
+
+    sushiDefaultCardPath = "default_cards/default_sushi.in"
     sushiInputCardPath = "Scan/"+str(mH)+"_"+str(mA)+".in"
     sushiOutputCardPath = "Scan/"+str(mH)+"_"+str(mA)+".out"
 
     # Replacements of variables into the input file
-    replacements = {'TANBETA':str(tb),'M12':str(m12),'MSMH':str(mh),'MHEAVYH':str(mH), 'MPSA':str(mA), 'MCHARGEDH':str(mhc), 'SINBA':str(sba), 'MUR':str(muR), 'MUF':str(muF)}
+    replacements = {'TANBETA':str(tb),
+                    'M12':str(m12),
+                    'MSMH':str(mh),
+                    'MHEAVYH':str(mH), 
+                    'MPSA':str(mA), 
+                    'MCHARGEDH':str(mhc), 
+                    'SINBA':str(sba), 
+                    'MUR4ggh':str(muR4ggh), 
+                    'MUF4ggh':str(muF4ggh),
+                    'MUR4bbh':str(muR4bbh), 
+                    'MUF4bbh':str(muF4bbh),
+                    }
     sushiDefaultCard = open(sushiDefaultCardPath)
     sushiInputCard = open(sushiInputCardPath, 'w')
     for line in sushiDefaultCard:
@@ -21,9 +34,8 @@ def getXsecFromSusHi( mode = 'H', sqrts = 13000, tb = 1, m12 = 0, mh = 125, mH =
     sushiInputCard.close()
 
     #running SusHi
-    run_sushi="./SusHi-1.5.0/bin/sushi "+sushiInputCardPath+" "+sushiOutputCardPath+" "
+    run_sushi="./SusHi-1.7.0/bin/sushi "+sushiInputCardPath+" "+sushiOutputCardPath+" "
     os.system(run_sushi)
-
 
     Xsec = None
     # extracting xsec from the output file
@@ -31,7 +43,7 @@ def getXsecFromSusHi( mode = 'H', sqrts = 13000, tb = 1, m12 = 0, mh = 125, mH =
         for line in f:
             if '# ggh XS in pb' not in f:
                 continue
-            print line.split()
+            print (line.split())
             Xsec = line.split()[1]
             break
     return Xsec 
